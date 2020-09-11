29879b758c587696241560bc2edb47c92505d211
pvpatil5 <pvpatil123@yahoo.com>
2020-09-11 12:23:13 +0530

    Third commit Modified class1 and class3 Added

--- src/pac/Class1.java
+++ src/pac/Class1.java
@@ -4,18 +4,7 @@ public class Class1
 {
 	public static void main(String[] args) {
 	
-		{
-			String s1= "Selenium";
-			String s2 = "";
-
-			char c[] = s1.toCharArray();
-
-			for (int i = c.length-1; i >=0; i--) 
-			{
-				s2= s2+c[i];
-			}
-			System.out.println(s2);
-		}
+		System.out.println("Class1");
 
 	}
 
---
+++ src/pac/Class3.java
@@ -0,0 +1,11 @@
+package pac;
+
+public class Class3 {
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("Class3");
+	}
+
+}
